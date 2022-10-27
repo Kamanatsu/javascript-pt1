143d38b1d4df952e5fb08e93d2515069f50f4cfa
Kamanatsu <109121926+Kamanatsu@users.noreply.github.com>
2022-10-27 11:53:56 -0300

    Add files via upload

--- questao6.js
+++ questao6.js
@@ -1,1 +1,14 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+number = 02546;
+
+for(let i = 0; i < number.length; i++){
+    if(number[i]%2 == 0){
+        contador++
+    };
+    if(contador == 2){
+        number.splice(number[i],0,'-');
+    }
+}
+
+console.log(number);
+
+
